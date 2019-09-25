4cbeb2b8dc3e21ed32c17d148606eb56f94b7246
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-09-25 20:34:51 -0300

    Add files via upload

---
+++ Ciccioli.Bruno/HacerLogin.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$archivo = fopen("registro.txt", "r");
+
+while(!feof($archivo)) 
+{
+$objeto = json_decode(fgets($archivo));
+if ($objeto->Usuario == $_GET['usuario']) 
+{	
+if ($objeto->Clave == $_GET['clave'])
+{
+header("Location: oklogin.php");
+fclose($archivo);
+exit();
+}
+else
+{
+header("Location: no.php");
+fclose($archivo);
+exit();
+}
+}
+
+
+}
+fclose($archivo);
+
+exit();
+
+?>
