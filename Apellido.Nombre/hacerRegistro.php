6c0d0df549d35fd9792fa851bd3f539adb3ddc33
Bruno Ciccioli <bciccioli@iiconcepcion.edu.ar>
2019-10-16 20:43:37 -0300

    Primer commit desde consola

--- Apellido.Nombre/hacerRegistro.php
+++
@@ -1,9 +0,0 @@
-<?php
-$miObjeto = new stdClass();
-$miObjeto->nombre = $_GET['nombre'];
-$miObjeto->apellido = $_GET['apellido'];
-
-$archivo = fopen('usuarios.txt', 'a');
-fwrite($archivo, json_encode($miObjeto)."\n");
-fclose($archivo);
-?>
--- Ciccioli.Bruno/Ciccioli.Bruno/hacerIngreso.php
+++
@@ -1,8 +0,0 @@
-<?php
-$MiObjeto= new stdClass();
-$MiObjeto->Patente=$_GET['patente'];
-$MiObjeto->Hora=$_GET['hora'];
-$archivo=fopen('estacionados.txt', 'a');
-fwrite($archivo,json_encode($MiObjeto)."\n");
-fclose($archivo);
-?>
